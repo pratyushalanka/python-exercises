234c7c92091be95a0fb681e53fef18169a6615aa
prathyusha_lanka <pratyusha.lanka999@gmail.com>
2025-12-10 17:05:27 +0530

    Add files via upload

---
+++ pgm.py
@@ -0,0 +1,168 @@
+"""s = int(input("enter a numb:"))
+square = s**2
+cube = s**3
+print(square)
+25
+print(cube)"""
+
+"""r = int(input("Enter a numb:"))
+h = int(input("Enter a numb:"))
+volume = 3.14*r**2*h
+print(volume)"""
+
+"""#Given the coordinates of two points (x1, y1) and (x2, y2). Write a program to find thedistance between these two points. 
+x1 = int(input("Enter x1: "))
+y1 = int(input("Enter y1: "))
+x2 = int(input("Enter x2: "))
+y2 = int(input("Enter y2: "))
+
+distance = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+
+print("Distance between the two points =", distance)"""
+"""
+# Write a program to check whether the given number is positive or negative. 
+n = int(input("Enter a numb:"))
+if n > 0:
+    print("positive number")
+else:
+    print("negative number")"""
+
+#Write a program to find out the given number is odd or even. 
+# 6
+# n = int(input("Enter a number:"))
+# if n % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(i,6):
+#         print(i,end=' ')
+#     print()
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=' ')
+#     print()
+
+# for i in range(5,0,-1):
+#     for k in range(5-i):
+#         print(" ",end= " ")
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+
+
+# for i in range(1,6):
+#     for j in range(i,6):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,6):
+#     for k in range(5-i):
+#         print(" ",end=" ")
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,i+1):
+#     for k in range(i):
+#         print(" ",end=" ")
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(5,0,-1):
+#     for j in range(i,6):
+#         print(i,end=" ")
+#     print()
+
+# for i in range(5,0,-1):
+#     for k in range(5-i):
+#         print(" ",end=" ")
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j, end=' ')
+#     print(1)
+
+# for i in range(1,6):
+#     for j in range(i,6):
+#         print(i, end=' ')
+#     print()
+
+# for i in range(5,0,-1):
+#     for j in range(1,i +1):
+#         print(i, end=' ')
+#     print()
+
+
+"""n = int(input("Enter n: "))
+
+list_of_numbers = []
+
+for i in range(1, n+1):
+    list_of_numbers.append(i) 
+
+print(list_of_numbers)
+
+"""
+
+# list_of_numbers = [0, 2, 3, 4, 5, 6, 7, 8, 9, [12, 34, 45, [12, 54, [100, 101]]],10, 11, 12, 12, 91]
+
+# print(list [7])
+
+# list_of_numbers.pop = [0, 2, 3, 4, 5, 6, 7, 8, 9, [12, 34, 45, [12, 54, [100, 101]]],10, 11, 12, 12, 91]
+
+# list_of_numbers[9][].pop(1)
+# print(list_of_numbers)
+
+
+
+
+
+
+# n1 = int(input())
+# n2 = int(input())
+# even_list = []
+# odd_list = []
+# for i in range(n1,n2+1):
+#         if i % 3 == 0: 
+#             if i % 2 == 0:
+#                  even_list.append(i)
+#             else:
+#                  odd_list.append(i)
+
+# print(even_list)
+# print(odd_list)
+
+# for i in range(1,6):
+#     for j in range(5,i,-1):
+#         print(' ',end=' ')
+#         for k in range(1,i+1):
+#             print(k,end=' ')
+#     print()
+
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j, end=' ')
+#     print()
+
+# l = [2, 5, 6, 2, 2, 6, 3, 7, 8, 5, 1, 6]
+# l1 = []
+# for x in l:
+#     if x not in l1:
+#         l1.append(x)
+# print(l1)
